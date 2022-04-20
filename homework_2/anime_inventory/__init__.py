be04b78a57c3b504b0af34bdb0910af26feb394d
faiber_barrero <faiberkoko95@gmail.com>
2022-04-20 03:30:50 -0400

    homework_w6

---
+++ homework_2/anime_inventory/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from config import Config
+from .site.routes import site
+from .authentication.routes import auth
+from .api.routes import api
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from.models import db as root_db
+
+
+app = Flask(__name__)
+
+app.register_blueprint(site)
+app.register_blueprint(auth)
+app.register_blueprint(api)
+
+app.config.from_object(Config)
+
+root_db.init_app(app)
+
+migrate = Migrate(app, root_db)
